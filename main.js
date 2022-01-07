8e76c66d61c8aa366caa0a0d57e5ac50f623bc35
SainaMehra <79200351+SainaMehra@users.noreply.github.com>
2022-01-07 19:59:44 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,1 +1,20 @@
 //https://teachablemachine.withgoogle.com/models/EuPbiB5Sx/
+function startClassification()
+{
+    navigator.mediaDevices.getUserMedia({audio:true});
+    classifier=ml5.soundClassifier("https://teachablemachine.withgoogle.com/models/EuPbiB5Sx/model.json",modelReady);
+}
+function modelReady()
+{
+    console.log("model is ready")
+    classifier.classify(gotResults);
+}
+function gotResults(error,results)
+{
+    if (error) {
+        console.log(error);
+    }
+    else {
+        console.log(results);
+    }
+}
