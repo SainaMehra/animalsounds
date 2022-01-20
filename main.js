f4b30a4c74e65b40a020a20a508573279900e4cf
SainaMehra <79200351+SainaMehra@users.noreply.github.com>
2022-01-20 20:16:41 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -16,5 +16,29 @@ function gotResults(error,results)
     }
     else {
         console.log(results);
+        random_number_r=Math.floor(Math.random() * 255)+1;
+        random_number_g=Math.floor(Math.random() * 255)+1;
+        random_number_b=Math.floor(Math.random() * 255)+1;
+        document.getElementById("result_label").innerHTML="I can hear - "+results[0].label;
+        document.getElementById("result_confidence").innerHTML="Accuracy - "+(results[0].confidence*100).toFixed(2)+"%";
+        document.getElementById("result_label").style.color="rgb("+random_number_r+","+random_number_g+","+random_number_b+")";
+        document.getElementById("result_confidence").style.color="rgb("+random_number_r+","+random_number_g+","+random_number_b+")";
+        if(results[0].label=="meowing")
+        {
+        img1.src="cat-pic.jpeg";
+        }
+        else if(results[0].label=="mooing")
+        {
+        img1.src="cow-pic.jpeg";
+        }
+        else if(results[0].label=="barking")
+        {
+        img1.src="dog-pic.jpeg";
+        }
+        else if(results[0].label=="roaring")
+        {
+        img1.src="lion-pic.jpeg";
+        }
     }
-}
+
+    }
